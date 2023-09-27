f78d7e041b79f85c4b4b04bf6c7d08bb079a4198
Ildar <ildarvagapov528@gmail.com>
2023-09-27 20:07:43 +0500

    f

--- src/pages/LocationPage.tsx
+++ src/pages/LocationPage.tsx
@@ -1,7 +1,6 @@
 import { Pagination } from "../components/Pagination/Pagination"
 import { useGetLocationQuery } from "../redux/api/api"
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 
 export const LocationPage = () => {
